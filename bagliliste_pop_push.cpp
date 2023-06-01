d3650ca75453988d1afa5241c36f460ebe111d3d
Samet <113308717+SametAkilli@users.noreply.github.com>
2023-06-01 14:57:55 +0300

    Update bagliliste_pop_push.cpp

--- bagliliste_pop_push.cpp
+++ bagliliste_pop_push.cpp
@@ -1,80 +1,71 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-struct Stu{
-	int ogrnumb;
-	int vize;
-	int final;
-	struct Stu *next;
+struct Node {
+    int ogrno;
+    int vize;
+    int final;
+    struct Node *sonraki;
 };
 
+struct Node *Push(struct Node *top, int ogrno, int vize, int final) {
+    struct Node *yeni = (struct Node*)malloc(sizeof(struct Node));
+    yeni->ogrno = ogrno;
+    yeni->vize = vize;
+    yeni->final = final;
+    yeni->sonraki = NULL;
+    
+    if (top == NULL) {
+        top = yeni;
+    } else {
+        yeni->sonraki = top;
+        top = yeni;
+    }
+    
+    return top;
+}
 
-struct Stu *Push(struct Stu *top , struct Stu ogrnumb , struct Stu vize , struct Stu final){
-		
-		struct Stu *yeni =(struct Stu*)malloc(1*sizeof(struct Stu*));
-		
-		yeni->ogrnumb = ogrnumb;
-		yeni->vize = vize;
-		yeni->final = final;
-		yeni->next=NULL;
-		if(top==NULL){
-			top=yeni;
-			return top;
-		}
-		else{
-			
-			yeni->next=top;
-			return new;
-			
-		}
-	}
-	
-	struct Stu *Pop(struct Stu *top , struct Stu *ogrnumb , struct Stu *vize, struct Stu *final){
-		if(top==NULL){
-			*ogrnumb=NULL;
-			*vize=NULL;
-			*final=NULL;
-			return top;
-		}
-		else{
-			struct Stu gecici*;
-			gecici=top;
-			
-			*ogrnumb-> gecici = ogrnumb;
-			*vize-> gecici = vize;
-			*final-> gecici =final;
-			
-			top= top->next;
-			free(gecici);
-			return top;
-		}
-	}
+struct Node *Pop(struct Node *top, int *ogrno, int *vize, int *final) {
+    if (top == NULL) {
+        *ogrno = 0;
+        *vize = 0;
+        *final = 0;
+        return top;
+    } else {
+        struct Node *gecici;
+        gecici = top;
+        
+        *ogrno = gecici->ogrno;
+        *vize = gecici->vize;
+        *final = gecici->final;
+        
+        top = top->sonraki;
+        free(gecici);
+        
+        return top;
+    }
+}
 
-int main (void) {
-	
-	struct Stu *top=NULL;
-	int ogrnumb,vize,final,i;
-	
-	for(i=0 ; i<3 ; i++){
-		
-		printf("%d. student -----------\n",i+1);
-		printf("Vize =");
-		scanf("%d",&vize);
-		printf("Final =");
-		scanf("%d",&final);
-		
-		top=Push(top,i+1,vize,final);
-	}
-	
-	printf("\n\n");
-	
-	for(i=0 ; i<3 ; i++){
-		
-		top Pop(top,ogrnumb,vize,final);
-		printf("ogr : %d vize/final : %d/%d"ogrnumb,vize,final);
-		
-	}
-	
-	
-	return 0;
+int main(void) {
+    struct Node *top = NULL;
+    int ogrno, vize, final, i;
+    
+    for (i = 0; i < 3; i++) {
+        printf("Ogr : %d ------------\n", i + 1);
+        printf("Vize = ");
+        scanf("%d", &vize);
+        printf("Final = ");
+        scanf("%d", &final);
+        
+        top = Push(top, i + 1, vize, final);
+    }
+    
+    printf("\n\n");
+    
+    for (i = 0; i < 3; i++) {
+        top = Pop(top, &ogrno, &vize, &final);
+        printf("Ogr : %d   vize/final = %d/%d\n", ogrno, vize, final);
+    }
+    
+    return 0;
 }
